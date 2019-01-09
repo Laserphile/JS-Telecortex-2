47bc8fe49e64eccabad5644e966e1edce3728628
derwentx <derwentx@gmail.com>
2019-01-09 17:33:07 +0800

    partially completed parseOPCBody
    is broken, but I want to show JVB

--- src/opc/parser.js
+++ src/opc/parser.js
@@ -1,12 +1,37 @@
 import { parse } from 'binary';
 import chalk from 'chalk';
 
+const OPC_HEADER_LEN = 4;
+
+/**
+ * parse header from OPC message.
+ * @return an object containing the channel, command and length
+ */
 const parseOPCHeader = msg => {
   return parse(msg)
     .word8u('channel')
     .word8u('command')
     .word16bu('length').vars;
 };
+
+/**
+ * parse body from OPC message.
+ * @return an array of colorsys RGB objects
+ */
+const parseOPCBody = msg => {
+  // skip over the message header
+  const body = msg.slice(OPC_HEADER_LEN);
+  const result = Array();
+  while (body.length > 2) {
+    result.push({
+      r: body.shift(),
+      g: body.shift(),
+      b: body.shift()
+    });
+  }
+  return result;
+};
+
 /**
  * parse OPC message and send data to spidevs
  */
@@ -21,4 +46,6 @@ export const handleOPCMessage = (context, msg) => {
     console.error(chalk`{red invalid channel ${header.channel} > ${spidevs.length}}`);
     return;
   }
+  const colours = parseOPCBody(msg);
+  console.log(`{bgMagenta.black  body: } ${colours}`);
 };
