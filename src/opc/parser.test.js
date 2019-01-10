9881b563b8f4dbc8ee06e58e1979b1ee5a609f2e
derwentx <derwentx@gmail.com>
2019-01-10 20:28:02 +1100

    🎨 use array of hex literals instead of string in tests
    avoids issue where string treats \x escapes as latin-1

--- src/opc/parser.test.js
+++ src/opc/parser.test.js
@@ -5,31 +5,31 @@ describe('parseOPCHeader', () => {
     expect(() => parseOPCHeader()).toThrow(Error);
   });
   it('handles short message', () => {
-    expect(() => parseOPCHeader(Buffer.from('\x00\x00'))).toThrow(Error);
+    expect(() => parseOPCHeader(Buffer.from([0x00, 0x00]))).toThrow(Error);
   });
   it('works with all null bytes', () => {
-    expect(parseOPCHeader(Buffer.from('\x00\x00\x00\x00'))).toEqual({
+    expect(parseOPCHeader(Buffer.from([0x00, 0x00, 0x00, 0x00]))).toEqual({
       channel: 0,
       command: 0,
       length: 0
     });
   });
   it('works with a small header', () => {
-    expect(parseOPCHeader(Buffer.from('\x01\x00\x00\x09'))).toEqual({
+    expect(parseOPCHeader(Buffer.from([0x01, 0x00, 0x00, 0x09]))).toEqual({
       channel: 1,
       command: 0,
       length: 9
     });
   });
   it('works with a big header (low bytes)', () => {
-    expect(parseOPCHeader(Buffer.from('\x0a\x00\x02\x01'))).toEqual({
+    expect(parseOPCHeader(Buffer.from([0x0a, 0x00, 0x02, 0x01]))).toEqual({
       channel: 10,
       command: 0,
       length: 513
     });
   });
   it('works with a big header (utf-8 fuckery)', () => {
-    expect(parseOPCHeader(Buffer.from('\xff\x00\x01\x00'))).toEqual({
+    expect(parseOPCHeader(Buffer.from([0xff, 0x00, 0x01, 0x00]))).toEqual({
       channel: 255,
       command: 0,
       length: 256
