40f827a763b2b81ae629b319af48b4c1e7069089
derwentx <derwentx@gmail.com>
2019-01-11 09:58:47 +1100

    🎨 reduce complexity of server
    fixes "Function server has 32 lines of code (exceeds 25 allowed)."

--- src/main.js
+++ src/main.js
@@ -9,48 +9,54 @@ if (process.platform === 'linux') {
   SPI = require('./util/testSpi').default;
 }
 
+const RPI_SPIDEVS = [
+  {
+    bus: 0,
+    device: 0
+  },
+  {
+    bus: 0,
+    device: 1
+  },
+  {
+    bus: 1,
+    device: 0
+  },
+  {
+    bus: 1,
+    device: 1
+  }
+];
+
+const DRV_CONF_DEFAULTS = {
+  // port used to listen for OPC commands
+  opc_port: 42069,
+  // Frame counter for FPS calculation
+  frames: 0,
+  // FPS rate calculated
+  rate: 0.0,
+  // time when script started for FPS calculation
+  start: now(),
+  // Last time something was printed
+  lastPrint: now(),
+  // eslint-disable-next-line no-unused-vars
+  brightness: 1
+};
+
 const server = () => {
   // const spi = SPI.initialize('./mntpoint/spidev0.0');
   // const spi = SPI.initialize('/dev/spidev0.0');
 
   // TODO: number of LEDS on each device?
-  const spidevs = [
-    {
-      bus: 0,
-      device: 0
-    },
-    {
-      bus: 0,
-      device: 1
-    },
-    {
-      bus: 1,
-      device: 0
-    },
-    {
-      bus: 1,
-      device: 1
-    }
-  ].map(spec => {
+  const spidevs = RPI_SPIDEVS.map(spec => {
     spec.spi = SPI.initialize(`/dev/spidev${spec.bus}.${spec.device}`);
     spec.spi.clockSpeed(5e5);
     return spec;
   });
 
   const driverConfig = {
-    spidevs,
-    // port used to listen for OPC commands
-    opc_port: 42069,
-    // Frame counter for FPS calculation
-    frames: 0,
-    // FPS rate calculated
-    rate: 0.0,
-    // time when script started for FPS calculation
-    start: now(),
-    // Last time something was printed
-    lastPrint: now(),
-    // eslint-disable-next-line no-unused-vars
-    brightness: 1
+    ...DRV_CONF_DEFAULTS,
+    spidevs
   };
   opcTCPServer(driverConfig);
   // opcUDPServer(driverConfig);
