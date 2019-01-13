af5d71363dc1c6fefe029450ba47f111e18a23f9
derwentx <derwentx@gmail.com>
2019-01-13 11:12:05 +1100

    🎨 🚨 reduce smelliness of parser tests

--- src/opc/parser.test.js
+++ src/opc/parser.test.js
@@ -7,32 +7,30 @@ describe('parseOPCHeader', () => {
   it('handles short message', () => {
     expect(() => parseOPCHeader(Buffer.from([0x00, 0x00]))).toThrow(Error);
   });
-  it('works with all null bytes', () => {
-    expect(parseOPCHeader(Buffer.from([0x00, 0x00, 0x00, 0x00]))).toEqual({
-      channel: 0,
-      command: 0,
-      length: 0
-    });
-  });
-  it('works with a small header', () => {
-    expect(parseOPCHeader(Buffer.from([0x01, 0x00, 0x00, 0x09]))).toEqual({
-      channel: 1,
-      command: 0,
-      length: 9
-    });
-  });
-  it('works with a big header (low bytes)', () => {
-    expect(parseOPCHeader(Buffer.from([0x0a, 0x00, 0x02, 0x01]))).toEqual({
-      channel: 10,
-      command: 0,
-      length: 513
-    });
-  });
-  it('works with a big header (utf-8 fuckery)', () => {
-    expect(parseOPCHeader(Buffer.from([0xff, 0x00, 0x01, 0x00]))).toEqual({
-      channel: 255,
-      command: 0,
-      length: 256
+  [
+    {
+      testName: 'works with all null bytes',
+      inArray: [0x00, 0x00, 0x00, 0x00],
+      out: { channel: 0, command: 0, length: 0 }
+    },
+    {
+      testName: 'works with a small header',
+      inArray: [0x01, 0x00, 0x00, 0x09],
+      out: { channel: 1, command: 0, length: 9 }
+    },
+    {
+      testName: 'works with a big header (valid latin-1 bytes)',
+      inArray: [0x0a, 0x00, 0x02, 0x01],
+      out: { channel: 10, command: 0, length: 513 }
+    },
+    {
+      testName: 'works with a big header (invalid latin-1 bytes)',
+      inArray: [0xff, 0x00, 0x01, 0x00],
+      out: { channel: 255, command: 0, length: 256 }
+    }
+  ].forEach(({ testName, inArray, out }) => {
+    it(testName, () => {
+      expect(parseOPCHeader(Buffer.from(inArray))).toEqual(out);
     });
   });
 });
@@ -48,15 +46,10 @@ describe('parseOPCBody', () => {
     expect(parseOPCBody(Buffer.from([0x00, 0x00, 0x00, 0x00]))).toEqual([]);
   });
   it('works with a typical body', () => {
-    expect(parseOPCBody(Buffer.from([
-        0x01, 0x00, 0x00, 0x09,
-        0xff, 0x00, 0x00,
-        0x00, 0xff, 0x00,
-        0x00, 0x00, 0xff
-    ]))).toEqual([
-      {r:255, g:0, b:0},
-      {r:0, g:255, b:0},
-      {r:0, g:0, b:255}
-    ]);
+    expect(
+      parseOPCBody(
+        Buffer.from([0x01, 0x00, 0x00, 0x09, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff])
+      )
+    ).toEqual([{ r: 255, g: 0, b: 0 }, { r: 0, g: 255, b: 0 }, { r: 0, g: 0, b: 255 }]);
   });
 });
