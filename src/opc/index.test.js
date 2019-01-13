7d54eb4ed021b0ff244f2caa212f194c86688530
derwentx <derwentx@gmail.com>
2019-01-13 14:10:58 +1100

    ✅ add test for invalid channel number

--- src/opc/index.test.js
+++ src/opc/index.test.js
@@ -13,9 +13,14 @@ describe('handleOPCMessage', () => {
       }
     ]
   };
+  const mockCalls = mockContext.spidevs[0].spi.transfer.mock.calls;
+  it('handles invalid channel', () => {
+    handleOPCMessage(mockContext, Buffer.from([0x01, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]));
+    expect(mockCalls.length).toBe(0);
+    // TODO: test console.error
+  });
   it('works', () => {
     handleOPCMessage(mockContext, Buffer.from([0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]));
-    const mockCalls = mockContext.spidevs[0].spi.transfer.mock.calls;
     expect(mockCalls.length).toBe(1);
     expect(mockCalls[0]).toMatchSnapshot();
   });
