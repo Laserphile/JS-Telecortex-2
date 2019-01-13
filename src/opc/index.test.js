4d8379ac8c1c76d16837916f1e62c8641242205a
derwentx <derwentx@gmail.com>
2019-01-13 20:49:40 +1100

    ✅ test multiple|partial|invalid messages in handleAllOPCMessages

--- src/opc/index.test.js
+++ src/opc/index.test.js
@@ -1,32 +1,95 @@
-import { handleOPCMessage } from './index';
+import { handleOPCMessage, handleAllOPCMessages } from './index';
 import 'jest';
 
-describe('handleOPCMessage', () => {
-  const mockContext = {
-    spidevs: [
-      {
-        spi: {
-          transfer: jest.fn()
-        },
-        bus: 0,
-        device: 0
-      }
-    ]
+const transferFn = jest.fn();
+
+const mockContext = {
+  spidevs: [
+    {
+      spi: {
+        transfer: transferFn
+      },
+      bus: 0,
+      device: 0
+    }
+  ]
+};
+
+/**
+ * Decorator which resets mockCalls after a callback
+ */
+const withTransferMockClear = callback => {
+  return () => {
+    callback();
+    transferFn.mockClear();
   };
-  const mockCalls = mockContext.spidevs[0].spi.transfer.mock.calls;
-  it('handles invalid channel', () => {
-    const data = Buffer.from([0x01, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]);
-    const bytesRead = handleOPCMessage(mockContext, data);
-    expect(mockCalls.length).toBe(0);
-    expect(bytesRead).toBe(data.length);
-    // TODO: test console.error
-    // clear calls
-    mockCalls.splice(0, mockCalls.length);
-  });
-  it('works', () => {
-    handleOPCMessage(mockContext, Buffer.from([0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]));
-    expect(mockCalls.length).toBe(1);
-    expect(mockCalls[0]).toMatchSnapshot();
-    mockCalls.splice(0, mockCalls.length);
-  });
+};
+
+describe('handleOPCMessage', () => {
+  it(
+    'handles invalid channel',
+    withTransferMockClear(() => {
+      const data = Buffer.from([0x01, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]);
+      const bytesRead = handleOPCMessage(mockContext, data);
+      expect(transferFn.mock.calls.length).toBe(0);
+      expect(bytesRead).toBe(data.length);
+      // TODO: test console.error
+    })
+  );
+  it(
+    'works',
+    withTransferMockClear(() => {
+      const data = Buffer.from([0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x00]);
+      const bytesRead = handleOPCMessage(mockContext, data);
+      expect(transferFn.mock.calls.length).toBe(1);
+      expect(transferFn.mock.calls[0]).toMatchSnapshot();
+      expect(bytesRead).toBe(data.length);
+    })
+  );
+});
+
+describe('handleAllOPCMessages', () => {
+  it(
+    'handles partial messages',
+    withTransferMockClear(() => {
+      const data = Buffer.from([0x00, 0x00, 0x00, 0x03, 0xff, 0x00]);
+      const response = handleAllOPCMessages(mockContext, data);
+      expect(transferFn.mock.calls.length).toBe(0);
+      expect(response).toBe(data);
+    })
+  );
+  it(
+    'handles invalid messages',
+    withTransferMockClear(() => {
+      const data = Buffer.from([0xff, 0x00, 0x00, 0x03, 0xff, 0x00]);
+      const response = handleAllOPCMessages(mockContext, data);
+      expect(transferFn.mock.calls.length).toBe(0);
+      expect(response).toBe(undefined);
+    })
+  );
+  it(
+    'handles multiple messages',
+    withTransferMockClear(() => {
+      const data = Buffer.from([
+        0x00,
+        0x00,
+        0x00,
+        0x03,
+        0xff,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x03,
+        0xff,
+        0x00,
+        0x00
+      ]);
+      const response = handleAllOPCMessages(mockContext, data);
+      expect(transferFn.mock.calls.length).toBe(2);
+      expect(transferFn.mock.calls).toMatchSnapshot();
+      expect(response).toBe(undefined);
+    })
+  );
 });
