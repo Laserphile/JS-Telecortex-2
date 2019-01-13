cc29bf1709c14656eba003238220055cded1c684
derwentx <derwentx@gmail.com>
2019-01-13 17:01:56 +1100

    🎨 update client example

--- src/client.js
+++ src/client.js
@@ -3,10 +3,16 @@ const net = require('net');
 const client = new net.Socket();
 client.connect(
   42069,
-  'localhost',
+  'raspberrypi.local',
   function() {
     console.log('Connected');
-    client.write('Hello, server! Love, Client.');
+    // client.write('Hello, server! Love, Client.');
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      client.write(
+        Buffer.from([0x03, 0x00, 0x00, 0x09, 0xff, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0xff])
+      );
+    }
   }
 );
 
