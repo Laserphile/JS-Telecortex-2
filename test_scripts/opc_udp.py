0c112758453f81afa91db3c34bf82bda0266abe3
derwentx <derwentx@gmail.com>
2019-01-15 10:25:23 +1100

    🐍 update python test scripts

--- test_scripts/opc_udp.py
+++ test_scripts/opc_udp.py
@@ -1,18 +1,18 @@
 import socket
 from kitchen.text.converters import to_bytes
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 42069
+DST_IP = "127.0.0.1"
+DST_PORT = 42069
 MESSAGE = "Hello, World!"
 
 def main():
-  print("UDP target IP:", UDP_IP)
-  print("UDP target port:", UDP_PORT)
+  print("destination IP:", DST_IP)
+  print("destination port:", DST_PORT)
   print("message:", MESSAGE)
 
   sock = socket.socket(socket.AF_INET,  # Internet
                       socket.SOCK_DGRAM)  # UDP
-  response = sock.sendto(to_bytes(MESSAGE), (UDP_IP, UDP_PORT))
+  response = sock.sendto(to_bytes(MESSAGE), (DST_IP, DST_PORT))
   print("response:", response)
 
 
