d3dfe5735049da7bb26b6cba69f1318db05622bd
derwentx <derwentx@gmail.com>
2019-01-17 09:58:12 +1100

    ⚗ Generate client connections asynchronously

--- src/client.js
+++ src/client.js
@@ -18,67 +18,81 @@ import net from 'net';
  */
 const serverConfigs = {
   1: {
-    ip: 'localhost',
+    host: 'raspberrypi.local',
     opc_port: 42069,
     channels: RPI_SPIDEVS
   }
 };
 
-Object.entries(serverConfigs).forEach(([serverID, serverConfig]) => {
-  console.log(`server ${serverID} config ${serverConfig}`);
+async function startClient(serverConfigs) {
+  const clients = await Promise.all(
+    Object.entries(serverConfigs).map(([serverID, serverConfig]) => {
+      return new Promise((resolve, reject) => {
+        const { host, opc_port } = serverConfig;
 
-  const { ip, opc_port, channels } = serverConfig;
-  const client = new net.Socket();
+        const client = new net.Socket();
 
-  const context = {
-    ...DRV_CONF_DEFAULTS,
-    channels,
-    frameRateCap: 30,
-    client
-  };
+        serverConfig.client = client;
 
-  const staticRainbowLoop = driverFactory(
-    context,
-    [
-      // singleRainbow,
-      rainbowFlow,
-      coloursToAllChannels,
-      colourRateLogger
-    ],
-    opcClientDriver
+        client.on('data', data => {
+          console.error(chalk`{cyan 📡 ${serverID} recieved} ${data.toString('hex')}`);
+          client.destroy(); // kill client after server's response
+        });
+
+        client.on('close', () => {
+          console.error(chalk`{cyan 📡 ${serverID} closed}`);
+        });
+
+        client.on('error', err => {
+          console.error(chalk`{cyan 📡 ${serverID} error} {white ${err}}`);
+          reject(err);
+        });
+
+        client.connect(
+          opc_port,
+          host,
+          () => {
+            console.log(chalk`{cyan 📡${serverID} connected} on port: {white ${opc_port}}`);
+            resolve(serverConfig);
+          }
+        );
+      });
+    })
   );
+  clients.forEach(serverConfig => {
+    const { client, channels } = serverConfig;
+    const context = {
+      ...DRV_CONF_DEFAULTS,
+      channels,
+      frameRateCap: 30,
+      client
+    };
 
-  /**
-   * Recursively schedules the frame function so that it is
-   * called at most frameRateCap times per second
-   */
-  const scheduleFrameRecursive = () => {
-    const startTimeMs = msNow();
-    staticRainbowLoop();
-    const delay = Math.max(0, 1000.0 / context.frameRateCap - (msNow() - startTimeMs));
-    // console.log(`scheduling for ${delay * 1000} ms`);
-    setTimeout(scheduleFrameRecursive, delay);
-  };
+    const staticRainbowLoop = driverFactory(
+      context,
+      [
+        // singleRainbow,
+        rainbowFlow,
+        coloursToAllChannels,
+        colourRateLogger
+      ],
+      opcClientDriver
+    );
 
-  client.on('data', function(data) {
-    console.error(chalk`{cyan 📡 Client recieved} ${data.toString('hex')}`);
-    client.destroy(); // kill client after server's response
+    /**
+     * Recursively schedules the frame function so that it is
+     * called at most frameRateCap times per second
+     */
+    const scheduleFrameRecursive = () => {
+      const startTimeMs = msNow();
+      staticRainbowLoop();
+      const delay = Math.max(0, 1000.0 / context.frameRateCap - (msNow() - startTimeMs));
+      // console.log(`scheduling for ${delay * 1000} ms`);
+      setTimeout(scheduleFrameRecursive, delay);
+    };
+
+    setTimeout(scheduleFrameRecursive, 1000);
   });
+}
 
-  client.on('close', function() {
-    console.error(chalk`{cyan 📡 Client closed}`);
-  });
-
-  client.on('error', function(err) {
-    console.error(chalk`{cyan 📡 Client error} {white ${err}}`);
-  });
-
-  client.connect(
-    opc_port,
-    ip,
-    function() {
-      console.log(chalk`{cyan 📡 Client connected} on port: {white ${opc_port}}`);
-      setTimeout(scheduleFrameRecursive, 1000);
-    }
-  );
-});
+startClient(serverConfigs);
