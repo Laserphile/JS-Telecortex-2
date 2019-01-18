583ad001a9b341791d95344e3794650b45c2278e
derwentx <derwentx@gmail.com>
2019-01-18 20:04:45 +1100

    ✅ add graphics tests

--- src/util/graphics.test.js
+++ src/util/graphics.test.js
@@ -1,5 +1,15 @@
-import { coloursToString, colourRateLogger } from './graphics';
+import {
+  coloursToString,
+  colourRateLogger,
+  rgbTocvPixelRaw,
+  cvPixelToRgb,
+  fillRainbows,
+  getSquareCanvas,
+  // setupMainWindow,
+  MAX_ANGLE
+} from './graphics';
 import { now } from '.';
+// const cv = require('opencv4nodejs');
 
 const someColours = [
   { r: 0xff, g: 0x00, b: 0x00 },
@@ -29,5 +39,36 @@ describe('colourRateLogger', () => {
     colourRateLogger(context);
     expect(console.log.mock.calls.length).toBe(1);
     expect(console.log.mock.calls[0]).toBeTruthy();
+  });
+});
+
+describe('rgbTocvPixelRaw', () => {
+  it('works', () => {
+    expect(rgbTocvPixelRaw({ r: 255, g: 128, b: 0 })).toEqual([0, 128, 255]);
+  });
+});
+
+describe('cvPixelToRgb', () => {
+  it('works', () => {
+    expect(cvPixelToRgb([0, 128, 255])).toEqual({ r: 255, g: 128, b: 0 });
+  });
+});
+
+describe('fillRainbows', () => {
+  it('works', () => {
+    const size = 64;
+    const img = getSquareCanvas(size);
+    // expect(img.atRaw(size / 2, size / 2)).toEqual([0, 0, 0]);
+    fillRainbows(img);
+    expect(img.atRaw(size / 2, size / 2)).toEqual([255, 231, 0]);
+  });
+  it('works with nonzero angle', () => {
+    const size = 64;
+    const img = getSquareCanvas(size);
+    // expect(img.atRaw(size / 2, size / 2)).toEqual([0, 0, 0]);
+    fillRainbows(img, MAX_ANGLE / 6);
+    expect(img.atRaw(size / 2, size / 2)).toEqual([255, 0, 24]);
+    // setupMainWindow(img);
+    // cv.waitKey();
   });
 });
