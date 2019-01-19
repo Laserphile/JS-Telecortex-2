20f2b695b0808ab9ff469098da254e894e8cca52
derwentx <derwentx@gmail.com>
2019-01-19 11:33:53 +1100

    ✅ 100% coverage of mapping

--- src/util/mapping.test.js
+++ src/util/mapping.test.js
@@ -2,9 +2,15 @@ import { normalizePixMap } from './mapping';
 
 const testPixMap = [[100, 100], [100, 300], [200, 100], [200, 300], [150, 200]];
 const expectPixMap = [[0, 0], [0, 1], [0.5, 0], [0.5, 1], [0.25, 0.5]];
+const badPixMap = [[1, 2, 3], [3]];
 
 describe('normalizePixelMap', () => {
   it('works', () => {
     expect(normalizePixMap(testPixMap)).toEqual(expectPixMap);
+  });
+  it('handles bad pixmap', () => {
+    expect(() => {
+      normalizePixMap(badPixMap);
+    }).toThrow();
   });
 });
