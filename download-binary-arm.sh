f4acd42d3895b52d6b01df6947c532dd53fca6f0
Jonathan Van Buren <vanbujm@gmail.com>
2019-05-29 14:54:47 +0800

    👷  fixing travis 50

--- download-binary-arm.sh
+++ download-binary-arm.sh
@@ -19,4 +19,4 @@ file $BINARY
 chmod a+x $BINARY
 unzip -q $BINARY
 rm -rdf node_modules/opencv4nodejs node_modules/opencv-build
-cp -R essential-build-artifacts/ node_modules/
+cp -R $BINARY/ node_modules/
