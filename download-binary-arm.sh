849e4955594297f6e95af7d6babd27401469c716
Jonathan Van Buren <vanbujm@gmail.com>
2019-05-30 17:30:13 +0800

    👷‍♀️  Getting travis to build the project 6

--- download-binary-arm.sh
+++ download-binary-arm.sh
@@ -1,45 +1,6 @@
 #!/bin/bash
 
 BASE_URL=https://github.com/Laserphile/JS-Telecortex-2/releases/download
-RELEASE=v0.1.1-alpha
-BINARY=essential-build-artifacts-arm.zip
-UNZIPPED_BINARY=essential-build-artifacts-arm
-
-if [[ -f $BINARY ]]
-then
-  echo "zip file found"
-  unzip -q $BINARY
-  rm -rdf node_modules/opencv4nodejs node_modules/opencv-build
-  cp -R $UNZIPPED_BINARY/ node_modules/
-  exit 0
-else
-  echo "$BINARY file not found"
-fi
-
-if [[ -d $UNZIPPED_BINARY ]]
-then
-  echo "folder found"
-  rm -rdf node_modules/opencv4nodejs node_modules/opencv-build
-  cp -R $UNZIPPED_BINARY/ node_modules/
-  exit 0
-else
-  echo "$UNZIPPED_BINARY folder not found"
-fi
-
-URL="$BASE_URL/$RELEASE/$BINARY"
-
-set -e
-echo "Fetching from: $URL"
-wget -q -O $BINARY "$URL"
-file $BINARY
-chmod a+x $BINARY
-unzip -q $BINARY
-rm -rdf node_modules/opencv4nodejs node_modules/opencv-build
-cp -R $UNZIPPED_BINARY/ node_modules/
-
-rm -rdf $UNZIPPED_BINARY
-
-BASE_URL=https://github.com/Laserphile/JS-Telecortex-2/releases/download
 RELEASE=v0.1.2-alpha
 BINARY=essential-build-artifacts-arm.zip
 UNZIPPED_BINARY=essential-build-artifacts-arm
